4e42a66bdf77d74b7bbd3fefe5ae471631c8665c
Yunix Dev <maryosilva1010@gmail.com>
2024-11-16 14:42:40 -0300

    Commit de correção da rota do backend

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crie uma instância do axios com configuração padrão
 const api = axios.create({
-  baseURL: 'app-forum-app-forum.up.railway.app', // Substitua pela URL base da sua API
+  baseURL: 'http://app-forum-app-forum.up.railway.app', // Substitua pela URL base da sua API
   headers: {
     'Content-Type': 'application/json',
   },
