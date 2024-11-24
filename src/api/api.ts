219c7f7003b3f7c5af2a9acde736099c3a579f9d
Yunix Dev <maryosilva1010@gmail.com>
2024-11-23 21:51:33 -0300

    primeiro commit frontend

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crie uma instância do axios com configuração padrão
 const api = axios.create({
-  baseURL: 'https://app-forum-app-forum.up.railway.app', // Substitua pela URL base da sua API
+  baseURL: 'http://localhost:3333', // Substitua pela URL base da sua API
   headers: {
     'Content-Type': 'application/json',
   },
