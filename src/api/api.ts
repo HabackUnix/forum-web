fd7a35a5d74841e8cecfee65104d1d8d6014eb37
Yunix Dev <maryosilva1010@gmail.com>
2024-11-23 22:03:37 -0300

    commit frontend

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Crie uma instância do axios com configuração padrão
 const api = axios.create({
-  baseURL: 'http://localhost:3333', // Substitua pela URL base da sua API
+  baseURL: 'https://sublime-sparkle-app-forum.up.railway.app/', // Substitua pela URL base da sua API
   headers: {
     'Content-Type': 'application/json',
   },
