ce14812038e2c7c89885d448b1779ec03c35e46c
Yunix Dev <maryosilva1010@gmail.com>
2024-11-25 16:22:02 -0300

    Importação no arquivo tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ export default {
   ],
   theme: {
     extend: {},
+    screens: {
+      'max-sm': '376px',
+      // => @media (min-width: 576px) { ... }
+
+      'md': '736px',
+      // => @media (min-width: 960px) { ... }
+
+      'lg': '928px',
+      // => @media (min-width: 1440px) { ... }
+    },
   },
   plugins: [
     require('tailwind-scrollbar'),
