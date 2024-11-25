9fec91a7ed2af1063acaf76db18d57c47f71c0d3
Yunix Dev <maryosilva1010@gmail.com>
2024-11-25 16:20:14 -0300

    correção da pagina MainComponent.tsx

--- src/components/AppComponent/MainComponent/MainComponent.tsx
+++ src/components/AppComponent/MainComponent/MainComponent.tsx
@@ -4,8 +4,8 @@ const MainComponent = () => {
   return (
     <div className="flex flex-row justify-center items-start w-full h-full">
       {/* Componente dos Posts com scroll */}
-      <div className="flex flex-col w-full lg:w-3/4 h-[32rem] overflow-y-auto scrollbar-none"> {/* Aumenta a altura para 32rem */}
-        <div className="space-y-4 p-2"> {/* Espaçamento vertical entre os posts */}
+      <div className=""> {/* Aumenta a altura para 32rem */}
+        <div className="space-y-4 p-2">
             <div>
             <PostComponent />
             </div>
